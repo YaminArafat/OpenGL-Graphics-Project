d6a7a647e90ce27c1562878b5867ec5fc32efa9c
arbir <abir1607098@gmail.com>
2021-05-18 19:00:32 +0600

    test commit

--- Shooting_Lab/main.cpp
+++ Shooting_Lab/main.cpp
@@ -1,16 +1,4 @@
-/*
- * GLUT Shapes Demo
- *
- * Written by Nigel Stewart November 2003
- *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
- *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
- */
+///Ashik_Abir 18/05/20
 
 #ifdef __APPLE__
 #include <GLUT/glut.h>
