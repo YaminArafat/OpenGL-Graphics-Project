17b15f933c4f485637c9d7eea0e18c1af5ec0293
YaminArafat <yaminarafat032@gmail.com>
2021-05-25 18:53:51 +0600

    setup

--- Shooting_Lab/main.cpp
+++ Shooting_Lab/main.cpp
@@ -1,18 +1,136 @@
-///Ashik_Abir 18/05/20
-
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <GL/glut.h>
-#endif
-
 #include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#include<math.h>
+#include "include/BmpLoader.h"
+const int windowWidth = 800;
+const int windowHeight = 800;
+GLfloat eyeX = 23;
+GLfloat eyeY = 15;
+GLfloat eyeZ = 0;
 
-static int slices = 16;
-static int stacks = 16;
+GLfloat lookX = 0;
+GLfloat lookY = 0;
+GLfloat lookZ = 0;
+unsigned int ID;
+bool light1= true, light2 = true, light3 =true;
+bool ambient =true, diffuse = true, specular = true;
+static GLfloat v_cube[8][3] =
+{
+    {0.0, 0.0, 0.0},
+    {0.0, 0.0, 1.0},
+    {0.0, 1.0, 0.0},
+    {0.0, 1.0, 1.0},
+    {1.0, 0.0, 0.0},
+    {1.0, 0.0, 1.0},
+    {1.0, 1.0, 0.0},
+    {1.0, 1.0, 1.0}
 
-/* GLUT callback Handlers */
+};
 
+static GLubyte quadIndices[6][4] =
+{
+    {0, 2, 6, 4},
+    {1, 5, 7, 3},
+    {0, 4, 5, 1},
+    {2, 3, 7, 6},
+    {0, 1, 3, 2},
+    {4, 6, 7, 5}
+};
+static void getNormal3p
+(GLfloat x1, GLfloat y1,GLfloat z1, GLfloat x2, GLfloat y2,GLfloat z2, GLfloat x3, GLfloat y3,GLfloat z3)
+{
+    GLfloat Ux, Uy, Uz, Vx, Vy, Vz, Nx, Ny, Nz;
+
+    Ux = x2-x1;
+    Uy = y2-y1;
+    Uz = z2-z1;
+
+    Vx = x3-x1;
+    Vy = y3-y1;
+    Vz = z3-z1;
+
+    Nx = Uy*Vz - Uz*Vy;
+    Ny = Uz*Vx - Ux*Vz;
+    Nz = Ux*Vy - Uy*Vx;
+
+    glNormal3f(Nx,Ny,Nz);
+}
+
+void LoadTexture(const char*filename)
+{
+    glGenTextures(1, &ID);
+    glBindTexture(GL_TEXTURE_2D, ID);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, ID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    BmpLoader bl(filename);
+    gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGB, bl.iWidth, bl.iHeight, GL_RGB, GL_UNSIGNED_BYTE, bl.textureData );
+}
+void drawCube(float colorRed=1, float colorGreen=1, float colorBlue=1, bool on=false)
+{
+    GLfloat no_mat[] = { 0, 0, 0, 1.0 };
+    GLfloat mat_ambient[] = { colorRed, colorGreen, colorBlue, 1.0 };
+    GLfloat mat_diffuse[] = { colorRed,colorGreen, colorBlue, 1.0 };
+    GLfloat mat_specular[] = { 1, 1, 1, 1.0 };
+    GLfloat mat_shininess[] = {50};
+
+    if(ambient)
+    {
+        glMaterialfv( GL_FRONT, GL_AMBIENT, mat_ambient);
+
+    }
+    else
+    {
+        glMaterialfv( GL_FRONT, GL_AMBIENT, no_mat);
+    }
+
+    if(diffuse)
+    {
+        glMaterialfv( GL_FRONT, GL_DIFFUSE, mat_diffuse);
+
+    }
+    else
+    {
+        glMaterialfv( GL_FRONT, GL_DIFFUSE, no_mat);
+    }
+
+    if(specular)
+    {
+        glMaterialfv( GL_FRONT, GL_SPECULAR, mat_specular);
+    }
+    else
+    {
+        glMaterialfv( GL_FRONT, GL_SPECULAR, no_mat);
+    }
+
+
+
+    glMaterialfv( GL_FRONT, GL_SHININESS, mat_shininess);
+
+    glBegin(GL_QUADS);
+    for (GLint i = 0; i <6; i++)
+    {
+        getNormal3p(v_cube[quadIndices[i][0]][0], v_cube[quadIndices[i][0]][1], v_cube[quadIndices[i][0]][2],
+                    v_cube[quadIndices[i][1]][0], v_cube[quadIndices[i][1]][1], v_cube[quadIndices[i][1]][2],
+                    v_cube[quadIndices[i][2]][0], v_cube[quadIndices[i][2]][1], v_cube[quadIndices[i][2]][2]);
+        glTexCoord2f(0,1);
+        glVertex3fv(&v_cube[quadIndices[i][0]][0]);
+        glTexCoord2f(0,0);
+        glVertex3fv(&v_cube[quadIndices[i][1]][0]);
+        glTexCoord2f(1,0);
+        glVertex3fv(&v_cube[quadIndices[i][2]][0]);
+        glTexCoord2f(1,1);
+        glVertex3fv(&v_cube[quadIndices[i][3]][0]);
+    }
+    glEnd();
+
+}
 static void resize(int width, int height)
 {
     const float ar = (float) width / (float) height;
@@ -28,57 +146,27 @@ static void resize(int width, int height)
 
 static void display(void)
 {
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
+
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    glFrustum(-5,5,-5,5, 2.0, 100.0);
+
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+    gluLookAt(eyeX,eyeY,eyeZ, lookX,lookY,lookZ, 0,1,0);
+
+    glViewport(0, 0, windowHeight, windowWidth);
 
     glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
+    glTranslatef(-40, -1, -25);
+    glScalef(70.0, 1.0, 60.0);
+    drawCube(1,1,1);
     glPopMatrix();
 
     glutSwapBuffers();
 }
-
 
 static void key(unsigned char key, int x, int y)
 {
@@ -90,16 +178,11 @@ static void key(unsigned char key, int x, int y)
             break;
 
         case '+':
-            slices++;
-            stacks++;
+
             break;
 
         case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
+
             break;
     }
 
@@ -111,15 +194,124 @@ static void idle(void)
     glutPostRedisplay();
 }
 
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
 
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
+static void light()
+{
+    GLfloat no_light[] = { 0.0, 0.0, 0.0, 1.0 };
+
+    GLfloat light_ambient[]  = {0.3, 0.3, 0.3, 1.0};
+    GLfloat light_diffuse[]  = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_position[] = { 2.5,19,13.5, 1.0 };
+
+    if(ambient)
+    {
+        glLightfv( GL_LIGHT0, GL_AMBIENT, light_ambient);
+
+    }
+    else
+    {
+        glLightfv( GL_LIGHT0, GL_AMBIENT, no_light);
+    }
+
+    if(diffuse)
+    {
+        glLightfv( GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+
+    }
+    else
+    {
+        glLightfv( GL_LIGHT0, GL_DIFFUSE, no_light);
+    }
+
+    if(specular)
+    {
+         glLightfv( GL_LIGHT0, GL_SPECULAR, light_specular);
+    }
+    else
+    {
+        glLightfv( GL_LIGHT0, GL_SPECULAR, no_light);
+    }
+
+    glLightfv( GL_LIGHT0, GL_POSITION, light_position);
+
+    GLfloat light_ambient2[]  = {0.5, 0.5, 0.5, 1.0};
+    GLfloat light_diffuse2[]  = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_specular2[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_position2[] = { 2.5,19,-14, 1.0 };
+
+    if(ambient)
+    {
+        glLightfv( GL_LIGHT1, GL_AMBIENT, light_ambient2);
+
+    }
+    else
+    {
+        glLightfv( GL_LIGHT1, GL_AMBIENT, no_light);
+    }
+
+    if(diffuse)
+    {
+        glLightfv( GL_LIGHT1, GL_DIFFUSE, light_diffuse2);
+
+    }
+    else
+    {
+        glLightfv( GL_LIGHT1, GL_DIFFUSE, no_light);
+    }
+
+    if(specular)
+    {
+         glLightfv( GL_LIGHT1, GL_SPECULAR, light_specular2);
+    }
+    else
+    {
+        glLightfv( GL_LIGHT1, GL_SPECULAR, no_light);
+    }
+
+    glLightfv( GL_LIGHT1, GL_POSITION, light_position2);
+
+    GLfloat light_ambient3[]  = {0.5, 0.3, 0.7, 1.0};
+    GLfloat light_diffuse3[]  = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_specular3[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_position3[] = { -0.5, 10, -0.5, 1.0 };
+
+    //glEnable( GL_LIGHT2);
+    if(ambient)
+    {
+        glLightfv( GL_LIGHT2, GL_AMBIENT, light_ambient3);
+
+    }
+    else
+    {
+        glLightfv( GL_LIGHT2, GL_AMBIENT, no_light);
+    }
+
+    if(diffuse)
+    {
+        glLightfv( GL_LIGHT2, GL_DIFFUSE, light_diffuse3);
+
+    }
+    else
+    {
+        glLightfv( GL_LIGHT2, GL_DIFFUSE, no_light);
+    }
+
+    if(specular)
+    {
+          glLightfv( GL_LIGHT2, GL_SPECULAR, light_specular3);
+    }
+    else
+    {
+        glLightfv( GL_LIGHT2, GL_SPECULAR, no_light);
+    }
+
+    glLightfv( GL_LIGHT2, GL_POSITION, light_position3);
+
+    GLfloat spot_direction[] = { 0.0, -15.0, 0.0 };
+    glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, spot_direction);
+    glLightf( GL_LIGHT2, GL_SPOT_CUTOFF, 60.0);
+}
 
 /* Program entry point */
 
@@ -130,34 +322,41 @@ int main(int argc, char *argv[])
     glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
-    glutCreateWindow("GLUT Shapes");
+    glutCreateWindow("Shooting Lab");
 
     glutReshapeFunc(resize);
     glutDisplayFunc(display);
     glutKeyboardFunc(key);
     glutIdleFunc(idle);
+    //glutIdleFunc(animate);
 
-    glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_LIGHT0);
+    glShadeModel( GL_SMOOTH );
+    glEnable( GL_DEPTH_TEST );
     glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_BLEND);
     glEnable(GL_LIGHTING);
 
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+//    glClearColor(1,1,1,1);
+//    glEnable(GL_CULL_FACE);
+//    glCullFace(GL_BACK);
+//
+//    glEnable(GL_DEPTH_TEST);
+//    glDepthFunc(GL_LESS);
+//
+//    glEnable(GL_LIGHT0);
+//    glEnable(GL_NORMALIZE);
+//    glEnable(GL_COLOR_MATERIAL);
+//    glEnable(GL_LIGHTING);
+//
+//    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+//    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+//    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+//    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+//
+//    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+//    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+//    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+//    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
 
     glutMainLoop();
 
